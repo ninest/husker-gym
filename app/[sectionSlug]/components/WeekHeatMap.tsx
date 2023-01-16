9d5b18d677932263c2e7650a574c1c0b7c1ff455
ninest <parth.kabra@gmail.com>
2023-01-15 20:42:22 -0500

    Componentize week heatmap

---
+++ app/[sectionSlug]/components/WeekHeatMap.tsx
@@ -0,0 +1,82 @@
+import { record } from "@prisma/client";
+
+export const WeekHeatMap = ({ records }: { records: record[] }) => {
+  // day is 0-indexed
+  const getFilteredRecords = (day: number, hour: number) => {
+    // Take into account for timezones
+    const offsetHours = new Date().getTimezoneOffset() / 60;
+    const hourTZ = hour - offsetHours;
+    const filteredRecords = records.filter(
+      (record) =>
+        record.time.getDay() === day &&
+        record.time.getHours() >= hourTZ &&
+        record.time.getHours() < hourTZ + 1
+    );
+    return filteredRecords;
+  };
+
+  const getAverageCount = (day: number, hour: number) => {
+    const filteredRecords = getFilteredRecords(day, hour);
+    const sum = filteredRecords.reduce((acc, rec) => acc + rec.count, 0);
+    return sum / filteredRecords.length;
+  };
+  const getAveragePercent = (day: number, hour: number) => {
+    const filteredRecords = getFilteredRecords(day, hour);
+    const sum = filteredRecords.reduce((acc, rec) => acc + rec.percent, 0);
+    return sum / filteredRecords.length;
+  };
+
+  const percentColorClass = (percent: number) => {
+    if (percent < 20) return "bg-blue-100";
+    else if (percent < 40) return "bg-blue-300";
+    else if (percent < 60) return "bg-blue-500";
+    else if (percent < 80) return "bg-blue-700";
+    else if (percent >= 80) return "bg-blue-900";
+    else return "";
+  };
+
+  const days = [
+    { name: "Sunday", shortName: "Sun", singleChar: "S" },
+    { name: "Monday", shortName: "Mon", singleChar: "M" },
+    { name: "Tuesday", shortName: "Tue", singleChar: "T" },
+    { name: "Wednesday", shortName: "Wed", singleChar: "W" },
+    { name: "Thursday", shortName: "Thu", singleChar: "R" },
+    { name: "Friday", shortName: "Fri", singleChar: "F" },
+    { name: "Saturday", shortName: "Sat", singleChar: "S" },
+  ];
+  const times = [
+    5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
+  ];
+
+  return (
+    <div>
+      {/* First row, days */}
+      <div className="grid grid-cols-8 gap-2 mb-3">
+        <div></div>
+        {days.map((day) => (
+          <div key={day.shortName} className="text-center">
+            {day.singleChar}
+          </div>
+        ))}
+      </div>
+
+      {times.map((time) => (
+        <div key={time} className="grid grid-cols-8 gap-1 mb-2">
+          <div className="text-right tabular-nums">
+            <span>{time.toString().padStart(2, "0")}</span>
+            <span>:00</span>
+          </div>
+
+          {days.map((day, dayIndex) => (
+            <div
+              key={dayIndex}
+              className={`rounded-md ${percentColorClass(
+                getAveragePercent(dayIndex, time)
+              )}`}
+            />
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
